2862588b89e5ba7746131a4beb1c49651ff19ad5
Jefri Tambunan <73419332+jefritambunan11@users.noreply.github.com>
2024-06-05 14:48:13 +0700

    update

--- Pointer/ex2/main.cpp
+++ Pointer/ex2/main.cpp
@@ -5,9 +5,8 @@ using namespace std;
 int main() {
   double high_value = 100;
   double low_value = 1;
-  double temp_value;
+  double *temp_value = nullptr; // to get safe 
   temp_value = &high_value;
-  temp_value = &low_value;
-  temp_value = nullptr; // to get safe  
+  temp_value = &low_value; 
 }
     
