307da3d5b466ac2d98d4d8354c8695ec71d109bf
Jefri Tambunan <73419332+jefritambunan11@users.noreply.github.com>
2024-06-05 14:52:49 +0700

    update

--- Pointer/ex2/main.cpp
+++ Pointer/ex2/main.cpp
@@ -6,7 +6,11 @@ int main() {
   double high_value = 100;
   double low_value = 1;
   double *temp_value = nullptr; // to get safe 
+  
   temp_value = &high_value;
+  cout << *temp_value << endl; // 100
+
   temp_value = &low_value; 
+  cout << *temp_value << endl; // 1
 }
     
