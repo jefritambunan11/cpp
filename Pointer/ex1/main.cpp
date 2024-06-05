4c72c56aa55e187e56dd91cd0861ecc7ae1c2ed7
Jefri Tambunan <73419332+jefritambunan11@users.noreply.github.com>
2024-06-05 14:44:37 +0700

    update

--- Pointer/ex1/main.cpp
+++ Pointer/ex1/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
   int score = 11;
   int *score_pointer = nullptr;
-  int high_value = 10;
-  score_pointer = &high_value;
+  double high_value = 100;
+  score_pointer = &high_value; // this is bad
 }
     
---
+++ Pointer/ex2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double high_value = 100;
+  double low_value = 1;
+  double temp_value;
+  temp_value = &high_value;
+  temp_value = &low_value;
+  temp_value = nullptr; // to get safe  
+}
+    
