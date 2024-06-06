fe131c729983e45dd75b6f3f8ffbf15deac17580
Jefri Tambunan <73419332+jefritambunan11@users.noreply.github.com>
2024-06-06 11:53:48 +0700

    update

--- Pointer/ex10/main.cpp
+++ Pointer/ex10/main.cpp
@@ -21,10 +21,18 @@ int main() {
   cout << num << endl;
   cout << shortCut << endl;
 
+  cout << "---------------------------\n" << endl;
+  vector<string> students = {"Larry", "Moe", "Currly"};
+  
+  for (auto std: students) std = "Funny"; // failed to change
+  for (auto std: students) cout << std << endl;
 
+  cout << "---------------------------\n" << endl;
+  for (auto &std: students) std = "Funny"; // succeed to change
+  for (auto const &std: students) cout << std << endl;
+  
   return 0;
 }
-
 
 /*  
 100
@@ -37,4 +45,14 @@ int main() {
 
 300
 300
+---------------------------
+
+Larry
+Moe
+Currly
+---------------------------
+
+Funny
+Funny
+Funny
 */
